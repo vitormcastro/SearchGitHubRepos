cb15049a63e71392d01a6aa29b74b5e7d74c2115
vitormcastro <vitormcastro@outlook.com>
2019-04-03 18:08:44 -0300

    Melhoria no visual e Controle de Exceção

--- src/index.js
+++ src/index.js
@@ -4,15 +4,38 @@ var app = new Vue({
     el: '#app',
     data () {
         return {
-            info: null,
-            message: 'vitormcastro'
+            nome: null,
+            avatarUrl: null,
+            bio: null,
+            perfilUrl: null,
+            login: '',
+            show: false,
+            falha: false,
+            messageFalha: null
         }
     },
     methods: {
         buscaGitAccount: function(){
-            axios.get('https://api.github.com/users/'+app.message)
-            .then(Response => (this.info = Response))
-            .catch(error => console.log(error))
+            axios.get('https://api.github.com/users/'+app.login)
+            .then(Response =>{
+                if(Response.data.name !== null){
+                    this.nome = Response.data.name;
+                }
+                else{
+                    this.nome = Response.data.login;
+                }
+                this.avatarUrl = Response.data.avatar_url;
+                this.bio = Response.data.bio;
+                this.show = true;
+                this.falha = false;
+                this.perfilUrl = Response.data.html_url;
+            })
+            .catch(error => {
+                console.log(error);
+                this.show = false;
+                this.falha = true;
+                this.messageFalha = error.response.data.message;
+            })
         }
     }
 });
